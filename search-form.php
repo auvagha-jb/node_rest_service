ea25538de75946e8f8b0879c6be13f6eab461ce6
auvagha_jb <jerry.auvagha@gmail.com>
2020-12-05 15:13:15 +0300

    Search form ui modification

--- search-form.php
+++ search-form.php
@@ -1,6 +1,22 @@
 <div class="container p-5">
+    <div class="row float-right">
+
+        <div class="px-1">
+            <button class="btn btn-primary" id="all-students-btn">
+                <i class="fa fa-users"></i> All Students
+            </button>
+        </div>
+
+        <!-- <div class="px-1">
+            <button class="btn btn-primary" id="all-courses-btn">
+                <i class="fa fa-graduation-cap"></i> All Courses
+            </button>
+        </div> -->
+    </div>
+
+
     <div class="row">
-        <div class="col-md-3">
+        <div class="col-md-4">
             <form class="form-inline" name="search-form" id="search-form">
                 <div class="input-group mb-2 mr-sm-2">
                     <input type="text" class="form-control" id="studentId" placeholder="Student Number">
@@ -13,15 +29,6 @@
                 </div>
             </form>
         </div>
-
-        <div class="col-md-9">
-            <div class="form-inline">
-                <button class="btn btn-primary" id="all-students-btn">
-                    <i class="fa fa-users"></i> All
-                </button>
-            </div>
-        </div>
-
     </div>
 
     <div id="search-results" class="py-3">
